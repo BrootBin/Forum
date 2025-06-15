bd78f3824d507a01bea24cc60a16460544477390
BrootBin <113306621+BrootBin@users.noreply.github.com>
2025-06-15 18:39:20 +0200

    Додав кнопку підтримки buymeacoffe

--- forum_project/settings.py
+++ forum_project/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'forum_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(conn_max_age=600)
+    'default': dj_database_url.config(
+        default=config("DATABASE_URL"),  # тут заміна
+        conn_max_age=600
+    )
 }
+
 
 
 
